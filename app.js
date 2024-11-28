cf21b1acf4b189766ef5770760557366b7e2f2b0
OmarElhagagy <oadeell21@gmail.com>
2024-11-28 20:27:53 +0200

    added req ad res for menu

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 5000;
 
 const homePage = fs.readFileSync('main.html');
 const aboutPage = fs.readFileSync('about.html');
+const menuPage = fs.readFileSync('menu.html');
+const css = fs.readFileSync('styles.css');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -14,6 +16,14 @@ const server = http.createServer((req, res) => {
     res.write(homePage);
   } else if (req.url === '/about') {
     res.write(aboutPage);
+  } else if(req.url === '/menu') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(menuPage);
+  } else if (req.url === '/styles.css') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/css');
+    res.write(css);
   } else if (req.url.match('/images/g')) {
     try {
       res.statusCode = 200;
